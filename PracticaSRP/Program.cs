540f5e9c79603129445f8038ebb3116628ea7520
GabrielaC7 <gabriela.corporan03@gmail.com>
2024-07-29 21:37:06 -0400

    Agregar archivos de proyecto.

---
+++ PracticaSRP/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Persona
+{
+    private string nombre;
+    private int edad;
+    private string direccion;
+    private string correoElectronico;
+
+    public Persona(string nombre, int edad, string direccion, string correoElectronico)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public void ImprimirDatos()
+    {
+        Console.WriteLine($"Nombre: {nombre}");
+        Console.WriteLine($"Edad: {edad}");
+        Console.WriteLine($"Dirección: {direccion}");
+        Console.WriteLine($"Correo electrónico: {correoElectronico}");
+    }
+
+    public string ObtenerCorreoElectronico()
+    {
+        return correoElectronico;
+    }
+}
+
+
+
---
+++ PracticaSRP/email.cs
@@ -0,0 +1,7 @@
+﻿public class EmailService
+{
+    public void EnviarCorreoElectronico(string direccion, string mensaje)
+    {
+        Console.WriteLine($"Enviando correo a {direccion}: {mensaje}");
+    }
+}
